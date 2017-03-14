a91c93b8aa360b6a0d270f05efc06ff1eb5d4baa
huiyu <huiyu@tethrnet.com>
2017-03-14 15:45:14 +0800

    add http handle

--- src/main/java/com/tethrnet/cloudmon/probe/HttpClientRunner.java
+++ src/main/java/com/tethrnet/cloudmon/probe/HttpClientRunner.java
@@ -1,9 +1,17 @@
 package com.tethrnet.cloudmon.probe;
 
 import io.vertx.core.Vertx;
-import io.vertx.core.http.HttpClient;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.http.HttpException;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+import java.io.IOException;
 
 /**
  * Created by huiyu on 17/3/14.
@@ -12,11 +20,11 @@ public class HttpClientRunner implements Runnable {
 
     protected Log log = LogFactory.getLog(HttpClientRunner.class);
     private Vertx vertx;
-    private HttpClient client;
+//    private HttpClient client;
 
     public HttpClientRunner(Vertx vertx) {
         this.vertx = vertx;
-        client = this.vertx.createHttpClient();
+//        client = this.vertx.createHttpClient();
     }
 
     @Override
@@ -25,7 +33,7 @@ public class HttpClientRunner implements Runnable {
         while (true) {
             try {
                 MsgQueue.getInstance().getHostMap().forEach((host, v) -> {
-                    startProbe(host, Constant.HTTP_SERVER_PORT);
+                    sendReq(host, Constant.HTTP_SERVER_PORT);
                 });
                 Thread.sleep(Constant.SEND_INTERVAL);
                 log.info("send http end");
@@ -35,16 +43,26 @@ public class HttpClientRunner implements Runnable {
         }
     }
 
-    private void startProbe(String host, int port) {
+//    private void startProbe(String host, int port) {
+//        try {
+//            client.getNow(port, host, "/", resp -> {
+//                log.info("send http request to " + host);
+//                log.debug("Got response " + resp.statusCode() + "; port: " + host);
+//                resp.bodyHandler(body -> {
+//                    log.debug("got data " + body.toString("ISO-8859-1"));
+//                });
+//            });
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
+
+    public void sendReq(String host, int port){
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet("http://" + host + ":"+ port);
         try {
-            client.getNow(port, host, "/", resp -> {
-                log.info("send http request to " + host);
-                log.debug("Got response " + resp.statusCode() + "; port: " + host);
-                resp.bodyHandler(body -> {
-                    log.debug("got data " + body.toString("ISO-8859-1"));
-                });
-            });
-        } catch (Exception e) {
+            CloseableHttpResponse response1 = httpclient.execute(httpGet);
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
